70208404696e6f04afab2b7f129eecb35d1d0b31
marko <maroza1111@gmail.com>
2018-03-05 14:25:40 +0100

    fix

--- ipBackend/src/main/java/com/controller/IpAddressController.java
+++ ipBackend/src/main/java/com/controller/IpAddressController.java
@@ -3,24 +3,40 @@ package com.controller;
 import com.entity.IpAddress;
 import com.service.IPAddressValidator;
 import com.service.IpAddressService;
+import com.service.StorageService;
+import com.strorage.StorageFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
-@RestController
+@Controller
 @RequestMapping("/addresses")
 public class IpAddressController {
 
-    @Autowired
+    private final StorageService storageService;
+
     private IpAddressService ipAddressService;
 
-    @Autowired
     private IPAddressValidator validator;
+
+    @Autowired
+    public IpAddressController(StorageService storageService, IpAddressService ipAddressService, IPAddressValidator validator) {
+        this.storageService = storageService;
+        this.ipAddressService = ipAddressService;
+        this.validator = validator;
+    }
 
     @RequestMapping(value = "/remote",method = RequestMethod.GET)
     public Collection<IpAddress> getAllRemoteIpAddress(){
@@ -43,4 +59,41 @@ public class IpAddressController {
             ipAddressService.postLocalIpAddress(ipAddress);
         }
     }
+
+    @GetMapping("/")
+    public String listUpLoadedFiles(Model model) throws IOException{
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(IpAddressController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
+        return "uploadForm";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+
+
 }
